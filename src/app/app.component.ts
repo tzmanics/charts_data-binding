92c9cec113ad91cbc2938eeece67e1b05c7c3ce5
tzmanics <tzmanics@gmail.com>
2016-11-28 10:55:30 -0500

    Add base styling and & core layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <h1>2015 US Census Data Visualized</h1>
+    <div class="visualization"></div>
+    <div class="visualization"></div>
+    <div class="visualization"></div>
+  `
 })
 export class AppComponent {
   title = 'app works!';
